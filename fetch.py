6f5b3cec8aee5a96212098cddc8fea688f63e9c9
danielballan <daniel.b.allan@gmail.com>
2014-06-20 09:36:00 -0400

    Initial commit

---
+++ fetch.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import os
+import boto
+
+def fetch(bucket_name, key_name):
+    """Fetch data from S3, and henceforth use a local copy.
+
+    The file will be sought in the path specified by the environmental variable
+    FETCH_STASH or, if that is not defined, it will be sought in the current
+    working directory.
+
+    If it is not found locally, fetch will download it from S3. The AWS
+    credentials AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be defined
+    as environemntal variables.
+
+    Parameters
+    ----------
+    bucket_name : string
+       identifing S3 bucket
+    key_name : string
+       file name ('key') of object to fetch
+
+    Example
+    -------
+    In [2]: fetch('my-bucket', 'some-file')
+    Fetching data from S3...
+    Out[2]: '/path/to/some-file'
+    """
+
+    stash_directory = os.environ.get('FETCH_STASH', '.')
+    path = os.path.join(stash_directory, key_name)
+    if os.path.isfile(path):
+        return path 
+    directory = os.path.dirname(path)
+    try: 
+        os.makedirs(directory)
+    except OSError:
+        if not os.path.isdir(directory):
+            raise
+    print("Fetching data from S3...")
+    boto.connect_s3().get_bucket(bucket_name).get_key(key_name).get_contents_to_filename(path)
+    return path
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='fetch',
+      version='1.0',
+      py_modules=['fetch'],
+      )
